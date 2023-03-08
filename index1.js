774e99f7708c94c398c7b7ced849b2bc166b9e38
R-1944 <119989538+R-1944@users.noreply.github.com>
2023-03-08 21:10:38 +0530

    Add files via upload
    javascript file

--- index1.js
+++ index1.js
@@ -8,10 +8,46 @@ function maketable(){
         for(var j=0;j<col;j++){
             var td=document.createElement('td');
             var text=document.createTextNode(" ");
+            document.getElementById('rows').value = "";
+            document.getElementById('cols').value = "";
             td.appendChild(text);
             tr.appendChild(td);
         }
         table.appendChild(tr);
+        
     }
 }
 document.getElementById('make').addEventListener("click",maketable);
+
+/*------------------------------colorPicker--------------------*/ 
+
+let selectedColor = document.getElementById('ColorPicker').value
+console.log("THIS IS THE COLOR ",selectedColor )
+
+/*--------------------change color-------------------*/ 
+
+function color_change(){
+    var color=document.getElementById('colorPicker').value;
+    
+}
+
+$("tr").mouseover(function(){
+    $("tr").css("background-color", "yellow");
+    cobl();
+  });
+
+
+function cobl()
+{
+ 
+    $("tr").css("background-color");
+}
+
+
+/*
+function color_select(){
+  var color = document.getElementById("#colorPicker");  
+  
+
+}
+document.getElementById("colorPicker").style.backgroundColor = color_select();*/
